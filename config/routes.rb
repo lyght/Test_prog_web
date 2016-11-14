e6f96ff588e47a0733280acf6ecb8fc2b3d84f6b
Gallien <w.gallien-93310@hotmail.fr>
2016-11-14 10:55:40 -0800

    Pages_controller

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get '/home', to: 'pages#home'
+
+  get '/about', to: 'pages#about'
+
+  get '/contact', to: 'pages#contact'
+
+  root 'pages#home'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
