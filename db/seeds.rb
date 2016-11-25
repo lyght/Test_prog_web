24924d365c43c1a4f7c5e3b8de26a467feef87cb
lyght <gwong@et.esiea.fr>
2016-11-25 13:22:54 +0100

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,2 @@
 Post.create!([
-  {title: "dfdsh", content: "sfdsf", author: "fsdsdf", image_file_name: nil, image_content_type: nil, image_file_size: nil, image_updated_at: nil},
-  {title: "dfgdfgg", content: "dgfd", author: "zsrezr", image_file_name: nil, image_content_type: nil, image_file_size: nil, image_updated_at: nil},
-  {title: "sqdaa", content: "azeaz", author: "eazeazeaz", image_file_name: nil, image_content_type: nil, image_file_size: nil, image_updated_at: nil},
-  {title: "Je t'enmerde lol", content: "Yolo", author: "Gallienne", image_file_name: nil, image_content_type: nil, image_file_size: nil, image_updated_at: nil},
-  {title: "test image", content: "lskjzeioeioz", author: "Galienne", image_file_name: "EQm39fW7.png", image_content_type: "image/png", image_file_size: 7538, image_updated_at: "2016-11-24 21:12:57"},
-  {title: "yui", content: "uuu", author: "uuu", image_file_name: nil, image_content_type: nil, image_file_size: nil, image_updated_at: nil},
-  {title: "testnew", content: "looll", author: "gallien", image_file_name: "EQm39fW7.png", image_content_type: "image/png", image_file_size: 7538, image_updated_at: "2016-11-24 21:40:59"}
 ])
